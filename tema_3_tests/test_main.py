8f570693b0eb011c32523d059cc1fbe976a6968f
dragosbajenaru1001 <155844538+dragosbajenaru1001@users.noreply.github.com>
2026-03-18 07:21:13 +0200

    Update test_main.py

--- tema_3_tests/test_main.py
+++ tema_3_tests/test_main.py
@@ -7,3 +7,9 @@ import pytest
 sys.stdout.reconfigure(encoding="utf-8")
 
 BASE_URL = "http://localhost:8000"
+
+#ToDo: Adăugați un test pentru endpoint-ul root
+
+#ToDo: Adăugați un scenariu de testare pentru endpoint-ul /chat/ care să fie evaluat de LLM as a Judge
+
+#ToDo: Adăugațu un test negativ pentru endpoint-ul /chat/ care să fie evaluat de LLM as a Judge
